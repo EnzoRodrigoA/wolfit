911b0c858c8cd66e2bbd379d9fc735b451dfbc2a
code.enzo <158048049+EnzoRodrigoA@users.noreply.github.com>
2025-08-12 17:36:27 +0000

    `eslint` fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,23 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 import jest from "eslint-plugin-jest";
 
+const jestGlobals = Object.fromEntries(
+  Object.entries(jest.environments.globals.globals).map(([key]) => [key, true]),
+);
+
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js, jest },
     extends: ["js/recommended"],
     languageOptions: {
-      globals: globals.browser,
-      ...jest.environments.globals.globals,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...jestGlobals,
+      },
     },
+    ignores: ["node_modules"],
     rules: {
       ...jest.configs["flat/recommended"].rules,
     },
