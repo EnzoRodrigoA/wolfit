b1370125b89d48a601312e85fda48a515e1cbc88
code.enzo <158048049+EnzoRodrigoA@users.noreply.github.com>
2025-08-12 16:30:34 +0000

    modify `eslint.config.mjs` to deal with jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import jest from "eslint-plugin-jest";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js, jest },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+      ...jest.environments.globals.globals,
+    },
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+    },
+  },
 ]);
