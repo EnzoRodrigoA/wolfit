372c87030e2edf52459a812bb333befc640efa9a
code.enzo <158048049+EnzoRodrigoA@users.noreply.github.com>
2025-08-26 10:09:31 -0300

    refactor: add and use `router.js` to center routes

--- src/server.js
+++ src/server.js
@@ -4,10 +4,7 @@ import dotenvExpand from "dotenv-expand";
 
 import logger from "./middlewares/logger.js";
 import globalErrorCatcher from "./middlewares/globalErrorCatcher.js";
-
-import StatusRoutesV1 from "./v1/routes/status.routes.js";
-import MigrationsRoutesV1 from "./v1/routes/migrations.routes.js";
-import UsersRoutesV1 from "./v1/routes/users.routes.js";
+import appRouter from "./v1/routes/appRouter.js";
 
 dotenvExpand.expand(dotenv.config({ path: ".env.development", quiet: true }));
 
@@ -17,9 +14,7 @@ const port = process.env.PORT || 3030;
 app.use(express.json());
 app.use(logger);
 
-app.use("/api/v1/status", StatusRoutesV1);
-app.use("/api/v1/migrations", MigrationsRoutesV1);
-app.use("/api/v1/users", UsersRoutesV1);
+app.use("/api/v1", appRouter);
 
 app.get("/", (request, response) => {
   response.status(200).json({ status: "Servidor rodando!" });
