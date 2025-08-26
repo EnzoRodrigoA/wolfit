5b600b795a38e533ab01a9a760e3989576512f46
code.enzo <158048049+EnzoRodrigoA@users.noreply.github.com>
2025-08-26 11:56:35 -0300

    refactor: add `timezone utc`

--- src/infra/migrations/1755872534502_create-users.js
+++ src/infra/migrations/1755872534502_create-users.js
@@ -22,12 +22,12 @@ export const up = (pgm) => {
     created_at: {
       type: "timestamptz",
       notNull: true,
-      default: pgm.func("now()"),
+      default: pgm.func("timezone('utc', now())"),
     },
     updated_at: {
       type: "timestamptz",
       notNull: true,
-      default: pgm.func("now()"),
+      default: pgm.func("timezone('utc', now())"),
     },
   });
 };
