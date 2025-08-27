4e3df61e8fa7910e1d6b7eed4415074231e666c7
code.enzo <158048049+EnzoRodrigoA@users.noreply.github.com>
2025-08-27 11:23:41 -0300

    feat: add support for `PATCH` on `/api/v1/users/[username]`

--- src/v1/routes/users.routes.js
+++ src/v1/routes/users.routes.js
@@ -6,6 +6,7 @@ import users from "../controllers/users/users.js";
 const router = Router();
 
 router.post("/", users.postHandler);
+router.patch("/:username", users.patchHandler);
 router.get("/:username", users.getOneByUsername);
 
 router.use("/", (request, response, next) => {
