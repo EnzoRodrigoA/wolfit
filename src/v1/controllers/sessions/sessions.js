7244f2dba95fab9d886b42d897f4cd4b8e87cf18
code.enzo <158048049+EnzoRodrigoA@users.noreply.github.com>
2025-08-31 11:44:55 -0300

    feat: implement session deletion

--- src/v1/controllers/sessions/sessions.js
+++ src/v1/controllers/sessions/sessions.js
@@ -21,8 +21,24 @@ async function postHandler(request, response, next) {
   }
 }
 
+async function deleteHandler(request, response, next) {
+  try {
+    const sessionToken = request.cookies.session_id;
+
+    const sessionObject = await session.findOneValidByToken(sessionToken);
+
+    const expiredSession = await session.expireById(sessionObject.id);
+    controller.clearSessionCookie(response);
+
+    return response.status(200).json(expiredSession);
+  } catch (error) {
+    next(error);
+  }
+}
+
 const users = {
   postHandler,
+  deleteHandler,
 };
 
 export default users;
