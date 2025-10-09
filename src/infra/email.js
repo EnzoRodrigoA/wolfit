53672cf0358dd63fb6726a19ce901f0fb3230803
code.enzo <158048049+EnzoRodrigoA@users.noreply.github.com>
2025-10-09 13:18:53 -0300

    fix: add `createTransformer` function to fix bugs

--- src/infra/email.js
+++ src/infra/email.js
@@ -1,16 +1,19 @@
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_SMTP_HOST,
-  port: process.env.EMAIL_SMTP_PORT,
-  auth: {
-    user: process.env.EMAIL_SMTP_USER,
-    pass: process.env.EMAIL_SMTP_PASSWORD,
-  },
-  secure: process.env.NODE_ENV === "production" ? true : false,
-});
+function createTransporter() {
+  return nodemailer.createTransport({
+    host: process.env.EMAIL_SMTP_HOST,
+    port: process.env.EMAIL_SMTP_PORT,
+    auth: {
+      user: process.env.EMAIL_SMTP_USER,
+      pass: process.env.EMAIL_SMTP_PASSWORD,
+    },
+    secure: process.env.NODE_ENV === "production" ? true : false,
+  });
+}
 
 async function send(mailOptions) {
+  const transporter = createTransporter();
   await transporter.sendMail(mailOptions);
 }
 
